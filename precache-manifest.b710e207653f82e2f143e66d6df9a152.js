3c5ec6235f1849f0358e37700a46e2a037a67d72
Haolin Yang <haolinyang95@gmail.com>
2019-10-28 14:24:39 -0400

    Updates

---
+++ precache-manifest.b710e207653f82e2f143e66d6df9a152.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4a72a8b77a6d7fe92a0a48b4adfa3e5",
+    "url": "/dommePokimonAPI/index.html"
+  },
+  {
+    "revision": "a52d636e1ec5f9991e70",
+    "url": "/dommePokimonAPI/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "8dce839b0303f79f9d8b",
+    "url": "/dommePokimonAPI/static/css/main.e303e48c.chunk.css"
+  },
+  {
+    "revision": "a52d636e1ec5f9991e70",
+    "url": "/dommePokimonAPI/static/js/2.02e105e7.chunk.js"
+  },
+  {
+    "revision": "8dce839b0303f79f9d8b",
+    "url": "/dommePokimonAPI/static/js/main.4fa7cc1c.chunk.js"
+  },
+  {
+    "revision": "60a9c5537eb3fe75f4c2",
+    "url": "/dommePokimonAPI/static/js/runtime-main.9b50b0ef.js"
+  },
+  {
+    "revision": "20bef8cfe0bb9179399a8fb7e2e77a30",
+    "url": "/dommePokimonAPI/static/media/logo.20bef8cf.png"
+  }
+]);
